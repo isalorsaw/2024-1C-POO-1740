1499ef4cadea1d2d9ed5d13f88f7de0e16948656
isalorsaw <isalorsaw@hotmail.com>
2024-02-28 18:18:09 -0600

    Intro con Arreglo 28/02

--- Pantallas/Escenario.java
+++ Pantallas/Escenario.java
@@ -3,12 +3,23 @@ import java.awt.Graphics;
 public class Escenario extends JPanel
 {
     Fondo f;
-    Yate y;
+    //Yate y;
+    Yate arreglo[]=new Yate[4];
     public Escenario()
     {
         f=new Fondo("imagenes/fondo2.png",0,0);//Inicializando el Fondo
-        y=new Yate("imagenes/yate_der.png",10,400);//Inicializando el Fondo
+        //y=new Yate("imagenes/yate_der.png",10,400);//Inicializando el Fondo
+        inicializarYates();
         setVisible(true);
+    }
+    public void inicializarYates()
+    {
+        int xx=10;
+        for(int i=0;i<arreglo.length;i++)
+        {
+            xx+=(i*60);
+            arreglo[i]=new Yate("imagenes/yate_der.png",xx,400);
+        }
     }
     public void paint(Graphics g)
     {
@@ -16,6 +27,15 @@ public class Escenario extends JPanel
         
         g.drawString("Elaborado por Ing Isaias Salinas",300,300);
         f.dibuja(g);//Dibujo el Fondo
-        y.dibuja(g);//Dibujo el Fondo
+        //y.dibuja(g);//Dibujo el Fondo
+        dibujarYates(g);
+    }
+    public void dibujarYates(Graphics g)
+    {
+        int xx=10;
+        for(int i=0;i<arreglo.length;i++)
+        {
+            arreglo[i].dibuja(g);
+        }
     }
 }
